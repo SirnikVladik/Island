36cd4ed288a9da54908694441b3f05673a2344ea
DESKTOP-7QT3942\Vladik <SirnikVladik@gmail.com>
2018-03-10 16:22:42 +0300

    Working

---
+++ main.js
@@ -0,0 +1,13 @@
+var menu = document.getElementById('#menu');
+var menuContent = document.querySelector('menu-content');
+
+
+new Vue({
+    el: "#body",
+    data: {
+        hide: 'true',
+    },
+    methods: {
+        
+    },
+});
