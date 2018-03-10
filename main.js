dffdee9b7906e510308daf879f605a988e073fe6
DESKTOP-7QT3942\Vladik <SirnikVladik@gmail.com>
2018-03-10 18:37:51 +0300

    working vue instance

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 var menu = document.getElementById('#menu');
 var menuContent = document.querySelector('menu-content');
+var size = document.getElementById('size');
 
+size.addEventListener('change', e => {
+    console.log(e.target.value);
+});
 
 new Vue({
     el: "#body",
     data: {
         hide: 'true',
+        size: 3,
     },
     methods: {
         hiding() {
             this.hide = !this.hide;
         },
+        changingSize(event) {
+            this.size = Number(event.target.value);            
+            console.log(this.size);
+        },
+        saveSettings(event) {
+            console.log(this.size);            
+        }
     },
 });
