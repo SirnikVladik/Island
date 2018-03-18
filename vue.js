216d0c26656d521ccad99dc9542d70f7f40bf95e
DESKTOP-7QT3942\Vladik <SirnikVladik@gmail.com>
2018-03-18 19:09:54 +0300

    Updates

--- vue.js
+++ vue.js
@@ -1,16 +1,26 @@
 var menu = document.getElementById('menu');
-var menuContent = document.querySelector('menu-content');
+var menuContent = document.querySelector('.menu-content');
 var size = document.getElementById('size');
+var blocks = document.getElementById('blocks');
 size.addEventListener('change', e => {
     console.log(e.target.value);
 });
 
 var bod = new Vue({
-    el: "#body",
+    el: "#wrapper",
     data: {
-        hide: 'true',
+        hide: true,
+        classObject: {
+            water: true,
+            sand: false,
+        },
         size: 3,
         sizeBefore: 3,
+        lines: [
+            [1, 0, 1],
+            [0, 1, 0],
+            [1, 0, 1]
+        ],
     },
     methods: {
         hiding() {
@@ -31,14 +41,26 @@ var bod = new Vue({
         },
         saveSettings(event) {
             this.sizeBefore = this.size;
-            console.log(this.size);
+            //this.lines = ;
         },
-        randomSelection(event){
+        randomSelection(event) {
             var s = this.size;
             this.size = Math.floor(Math.random() * 10) + 1;
-            if(this.size == s) {
+            if (this.size == s) {
                 bod.randomSelection();
             };
+        },
+        changeMaterial(e) {
+            let target = e.target;
+            if (target.classList.contains('water')) {
+                target.classList.remove('water');
+                target.classList.add('sand');
+                console.log('Material chenged to sand');
+            } else if (target.classList.contains('sand')) {
+                target.classList.remove('sand');
+                target.classList.add('water');
+                console.log('Material chenged to water');
+            }
         },
     },
 });
