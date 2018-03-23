1284bbe6fb32d234d5dbc4eb699bfc8ccc4fe7b4
DESKTOP-7QT3942\Vladik <SirnikVladik@gmail.com>
2018-03-23 19:39:07 +0300

    Working size

--- vue.js
+++ vue.js
@@ -5,22 +5,22 @@ var blocks = document.getElementById('blocks');
 size.addEventListener('change', e => {
     console.log(e.target.value);
 });
+var startWidth = String(Number(blocks.offsetWidth)/3) + 'px';
 
 var bod = new Vue({
     el: "#wrapper",
     data: {
+        islands: 0,
         hide: true,
-        classObject: {
-            water: true,
-            sand: false,
-        },
         size: 3,
         sizeBefore: 3,
         lines: [
-            [1, 0, 1],
-            [0, 1, 0],
-            [1, 0, 1]
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
         ],
+        width: Number(blocks.offsetWidth),
+        height: startWidth,
     },
     methods: {
         hiding() {
@@ -32,35 +32,51 @@ var bod = new Vue({
             if (val < 1) {
                 val = String(1);
                 this.size = 1;
-            } else if (val > 10) {
-                val = String(10);
-                this.size = 10;
+            } else if (val > 9) {
+                val = String(9);
+                this.size = 9;
             } else {
                 this.size = Number(event.target.value);
-            }
+        }
         },
         saveSettings(event) {
             this.sizeBefore = this.size;
-            //this.lines = ;
+            let k = 0;
+            this.lines = [];
+            for (let i = 0; i < this.size; i++) {
+                this.lines.push([]);
+                for(let j = 0; j < this.size; j++) {
+                    this.lines[i].push(k);
+                }
+            };
+            this.height = String(this.width/this.size) + 'px';
         },
         randomSelection(event) {
-            var s = this.size;
-            this.size = Math.floor(Math.random() * 10) + 1;
-            if (this.size == s) {
-                bod.randomSelection();
+            this.sizeBefore = this.size;
+            this.lines = [];
+            for (let i = 0; i < this.size; i++) {
+                this.lines.push([]);
+                for(let j = 0; j < this.size; j++) {
+                    let k = Math.floor(Math.random() * 2);
+                    this.lines[i].push(k);
+                }
             };
+            this.height = String(this.width/this.size) + 'px';
         },
         changeMaterial(e) {
             let target = e.target;
             if (target.classList.contains('water')) {
+                this.lines[target.textContent[0] - 1][target.textContent[2] - 1] = 1;
                 target.classList.remove('water');
                 target.classList.add('sand');
                 console.log('Material chenged to sand');
             } else if (target.classList.contains('sand')) {
+                this.lines[target.textContent[0] - 1][target.textContent[2] - 1] = 0;
                 target.classList.remove('sand');
                 target.classList.add('water');
                 console.log('Material chenged to water');
-            }
+            };
+            console.log(this.lines);            
         },
     },
 });
